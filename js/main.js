4102ceabefad381230b0a5434431826f06711b6d
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2021-12-16 10:29:36 -0800

    v1

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// function changeRandomValues() {
+// 	document.documentElement.style.setProperty("--random-rotation", (Math.floor(Math.random() * 10) - 5) + "deg");
+// }
+
+// setInterval(changeRandomValues, 250);
