b8feb9711a5ec426b636923f5ea7974c995c7478
Kyle <41976328+ImKyle4815@users.noreply.github.com>
2023-01-24 09:58:04 -0800

    Better compiler

--- src/askUrzaCompiler/compile.mjs
+++ src/askUrzaCompiler/compile.mjs
@@ -4,17 +4,37 @@ const plus = [];
 const minus = [];
 const ultimate = [];
 
-fs.readFile("./oracle-cards.json", "utf8", (err, file) => {
+fs.readFile("oracle-cards.json", "utf8", (err, file) => {
     if (err) {
         console.log("An error occured:", err);
     } else {
         // fs.readFile
         for (let card of JSON.parse(file)) {
-            if (!card["type_line"].includes("Planeswalker")) continue;
+            if (!card["type_line"].includes("Planeswalker") || !card["oracle_text"]) continue; // || card.legalities?.commander !== "legal"
             for (let ability of card["oracle_text"].split(/\n/g)) {
-                console.log(ability)
+                const splitAbility = ability.split(":", 2);
+                if (splitAbility[0].length > 3 || splitAbility[0] === "-X") {
+                    continue;
+                } else {
+                    const abilityCost = parseInt(splitAbility[0].replace("−", "-").replace("+", ""));
+                    const abilityText = splitAbility[1].slice(1).replace(card.name, "Urza, Academy Headmaster");
+                    if (abilityCost >= 1) {
+                        plus.push(abilityText);
+                    } else if (abilityCost <= -3) {
+                        ultimate.push(abilityText);
+                    } else {
+                        minus.push(abilityText);
+                    }
+                }
             }
-            return;
         }
+        const output = `const plus = ${JSON.stringify(plus)}; const minus = ${JSON.stringify(minus)}; const ultimate = ${JSON.stringify(ultimate)};`;
+        fs.writeFile("./output.txt", output, "utf8", (err) => {
+            if (err) {
+                console.log("Failed to write output.txt:", err);
+            } else {
+                console.log("Output saved to output.txt");
+            }
+        });
     }
-})
+});
